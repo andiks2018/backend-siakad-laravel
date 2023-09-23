713943f47abd1226b06b69e23dec6070d4699f6e
andi <andiks2018@gmail.com>
2023-09-23 17:13:01 +0700

    pages dan home sudah ready

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; ANDI ISMAIL 2023
+  </div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2023 <div class="bullet"></div> @ <a href="https://nauval.in/">Andi Ismail</a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
