2fd4a1f7d4e4029f3855763adf754d7bf6f4384f
andi <andiks2018@gmail.com>
2023-09-24 13:27:34 +0700

    auth login, register, home

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
+Route::middleware(['auth'])->group(function(){
+    Route::get('home', function(){
+        return view('pages.app.dashboard-siakad', ['type_menu'=>'']);
+    })->name('home');
+});
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})->name('register');
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('forgot');
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('forgot');
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('reset-passwors');
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset-passwors');
